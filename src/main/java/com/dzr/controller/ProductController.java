a2329c4910a8d8a16317dbf5a3544bcd170b73ae
Administrator <myliveding@163.com>
2017-08-20 21:10:39 +0800

    添加几个接口的处理

--- src/main/java/com/dzr/controller/ProductController.java
+++ src/main/java/com/dzr/controller/ProductController.java
@@ -1,7 +1,9 @@
 package com.dzr.controller;
 
 import com.dzr.framework.config.Constant;
+import com.dzr.framework.config.InvokingUrl;
 import net.sf.json.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,13 +22,16 @@ import javax.servlet.http.HttpServletRequest;
 @RequestMapping("/product")
 public class ProductController {
 
+    @Autowired
+    InvokingUrl invokingUrl;
+
     @RequestMapping("/productCates")
     public String getproductCates(Model model) {
 
         //获取产品分类
         String[] arr = new String[]{};
         String mystr = "";
-        JSONObject result = JSONObject.fromObject(Constant.getInterface(Constant.PRODUCT_CATES, mystr, arr));
+        JSONObject result = JSONObject.fromObject(Constant.getInterface(invokingUrl.getPhp() + Constant.PRODUCT_CATES, mystr, arr));
         if (0 == result.getInt("error_code")) {
             model.addAttribute("cates", result.getJSONArray("result"));
         }
@@ -41,7 +46,7 @@ public class ProductController {
         //获取产品列表
         String[] arr = new String[]{"cate_id" + cateId, "per_page" + perPage, "page" + page};
         String mystr = "cate_id=" + cateId + "&per_page=" + perPage + "&page=" + page;
-        JSONObject info = JSONObject.fromObject(Constant.getInterface(Constant.PRODUCTS, mystr, arr));
+        JSONObject info = JSONObject.fromObject(Constant.getInterface(invokingUrl.getPhp() + Constant.PRODUCTS, mystr, arr));
         if (0 == info.getInt("error_code")) {
             model.addAttribute("products", info.getJSONArray("result"));
         }
@@ -54,7 +59,7 @@ public class ProductController {
         //获取产品详情
         String[] arr = new String[]{"product_id" + productDetail};
         String mystr = "product_id=" + productDetail;
-        JSONObject productInfo = JSONObject.fromObject(Constant.getInterface(Constant.PRODUCT_DETAIL, mystr, arr));
+        JSONObject productInfo = JSONObject.fromObject(Constant.getInterface(invokingUrl.getPhp() + Constant.PRODUCT_DETAIL, mystr, arr));
         if (0 == productInfo.getInt("error_code")) {
             model.addAttribute("info", productInfo.getJSONObject("result"));
         }
